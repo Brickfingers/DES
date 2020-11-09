204396ae6b85ac4c59d17c1490ff5c3fa5248a00
Robert Wright <wrig0410@algonquinlive.com>
2020-11-08 23:44:18 -0500

    moved des.h first stab at controller.c

--- src/des_controller.c
+++ src/des_controller.c
@@ -1,6 +1,6 @@
 /**
- * Nick Wright
- * 040911884
+ * Nick Wright & Kevin Luu
+ * 040911884 & 040905303
  * CST8244
  */
 
@@ -18,13 +18,55 @@ int main(void) {
 	int rcvid;
 	int chid;
 
+	//SERVER SETUP
+
 	chid = ChannelCreate(0);
 	if (chid == -1) {
 		fprintf(stderr, "ChannelCreate failure.\n");
 		exit(EXIT_FAILURE);
 	}
-
 	printf("controller PID is %d\tAnd the Channel ID (chid) is: %d\n", getpid(),
 			chid);
 
+	//CLIENT SETUP
+
+	//On Failure: print usage message and EXIT_FAILURE
+	if (argc != 2) {
+		//print usage message
+		fprintf(stderr,
+				"Usage: ./des_inputs <pid of des_display> <enter>\n");
+		exit(EXIT_FAILURE);
+	}
+	//Phase I
+	//Get controller's PID from command-line arguments.
+	serverpid = atoi(argv[1]);
+
+	coid = ConnectAttach(ND_LOCAL_NODE, serverpid, 1, _NTO_SIDE_CHANNEL, 0);
+	if (coid == -1) {
+		fprintf(stderr, "ConnectAttach had an error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//PHASE II receive message & process message
+	//SERVER WAIT FOR MESSAGE -> pass on to client
+
+	while (1) {
+
+		//get message
+		rcvid = MsgReceive(chid, &client_message, sizeof(client_message), NULL);
+
+		if (rcvid == -1) {
+			fprintf(stderr, "MsgReceive error.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		//do stuff
+		//pass on message to des_display
+
+	}
+
+	//PHASE III
+	ChannelDestroy(chid);
+	ConnectDetach(coid);
+	exit(EXIT_SUCCESS);
 }
